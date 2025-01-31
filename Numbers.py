6ef873dd449298761286c4bcdaf992b4f3749466
Rajesh Shanbhag <rajesh@veritysolutions.com.au>
2025-01-31 19:38:38 +1100

    Message1

---
+++ Numbers.py
@@ -0,0 +1,14 @@
+print (5+3)
+print (5-2)
+print (5*2)
+print (5/2)
+print (10//3)            # floor division truncates decimals
+print (5%2)             # modulo = remiander
+print (5**5)             # power
+print (5**0.5)          # root
+print (5+2*2+5)
+print ((5+2)*(2+5))
+print(6-5.7)                # floating num
+print(20+7)
+print(20-5)
+print("first")
